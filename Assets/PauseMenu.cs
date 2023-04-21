615ba0759deed36a8962db2baad374309a798890
Journalax <119070166+Journalax@users.noreply.github.com>
2023-04-21 11:15:45 +0545

    Revert "Revert "fixed pause and slow mo added""
    This reverts commit d8d4e103dc73b5b30160468164624ee6ff4ba88d.

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,63 +1,85 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class PauseMenu : MonoBehaviour
 {
-    
 
+    [SerializeField] private GameObject pauseMenu;
+    private bool IsPaused = false;
+    private bool IsSlowedDown = false;
 
-    public GameObject pauseMenu;
-    public  bool IsPaused;
-    // Start is called before the first frame update
-
+    private Coroutine c_timer; 
      
-    void Start()
-    {
-     pauseMenu.SetActive(false);
-    }
+    void Start() => pauseMenu.SetActive(false);
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-          
-           if(IsPaused)
-           {
-          
-            ResumeGame();
-           }
-           else
-           {
-            PauseGame();
-           }
-        }
+        if(Input.GetKeyDown(KeyCode.LeftShift)) SlowDow();
+        if(!Input.GetKeyDown(KeyCode.Escape)) return;
+        if(IsPaused) ResumeGame();
+        else if(!IsPaused) PauseGame();
     }
+
     public void PauseGame(){
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        PauseGame ();
         IsPaused=true;
-
-
+        UpdateTimeScale(0f);
+        pauseMenu.SetActive(true);
     }
+
     public void ResumeGame(){
-         pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
         IsPaused=false;
+        if(c_timer != null) StopCoroutine(c_timer);
+        c_timer = StartCoroutine(ScaleTime(0f,1f,2f));
+
+        pauseMenu.SetActive(false);
 
     }
+
     public void GoToMainMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Mainmenu");
+        SceneManager.LoadScene("Home");
         IsPaused = false;
     }
 
-    public void quit(){
-    Application.Quit();
-   }
+    public void SlowDow(){
+        if(IsSlowedDown)
+        {
+            IsSlowedDown = false;
+            if(c_timer != null)StopCoroutine(c_timer);
+            c_timer = StartCoroutine(ScaleTime(.2f, 1f, 3f));
+        }
 
+        else if(!IsSlowedDown)
+        {
+            IsSlowedDown = true;
+            if(c_timer != null)StopCoroutine(c_timer);
+            c_timer = StartCoroutine(ScaleTime(1f, .2f, 3f));
+        }
+    }
+
+    public static IEnumerator ScaleTime(float start, float end, float time)
+    {
+        float lastTime = Time.realtimeSinceStartup;
+        float timer = 0.0f;
+
+        while (timer < time)
+        {
+            Time.timeScale = Mathf.Lerp(start, end, timer / time);
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+            timer += (Time.realtimeSinceStartup - lastTime);
+            lastTime = Time.realtimeSinceStartup;
+            yield return null;
+        }
+        Time.timeScale = end;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+
+    public void UpdateTimeScale(float timeScale){
+        Time.timeScale = timeScale;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+
+    public void quit() => Application.Quit();
     
 }
